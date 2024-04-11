25cd6eae8d09a2dd39b92706345283a85c2b9c14
wenmianshi <134840821+wenmianshi@users.noreply.github.com>
2024-04-11 22:46:49 +0200

    update: comments

--- XiaoMao/Program.cs
+++ XiaoMao/Program.cs
@@ -2,6 +2,7 @@
 
 internal class Program
 {
+    // This Data is used for the JSON from the REST-API
     public class CharacterData
     {
         public string? Name { get; set; }
@@ -30,7 +31,7 @@ internal class Program
             return;
         }
 
-        // Creating table clients
+        // Creating table characters
         NpgsqlCommand command = new NpgsqlCommand(
             @"CREATE TABLE IF NOT EXISTS characters(
             characterid serial PRIMARY KEY,
@@ -44,10 +45,10 @@ internal class Program
         NpgsqlDataReader dataReader = command.ExecuteReader();
         dataReader.Close();
 
-        // Check if table clients exists now
+        // Check if table characters exists now (all tables will be created in pg_tables)
         command.CommandText = @"SELECT EXISTS (
         SELECT FROM 
-            pg_tables
+            pg_tables 
         WHERE 
             schemaname = 'public' AND 
             tablename  = 'characters'
@@ -62,9 +63,9 @@ internal class Program
             Console.WriteLine("Table characters doesn't exist. Stopping program.");
             return;
         }
-        dataReader.Close();
+        dataReader.Close(); 
 
-        // Requesting some characters
+        // Requesting some characters from REST API
         HttpClient client = new HttpClient();
         client.BaseAddress = new Uri("https://harry-potter-api-3a23c827ee69.herokuapp.com/api/characters");
         string parameters = "";
@@ -72,8 +73,13 @@ internal class Program
             new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json")
            );
 
-        string values = @"";
-        HttpResponseMessage response = client.GetAsync(parameters).Result;
+        // Now we want to build the values so we can add them to our table characters
+        // The values should look like this:
+        // ('Wenmian', 'Shi', '2005-11-01', 'someImg.img'),
+        // ('Pascal', 'Lefev', '2005-11-01', 'someImg.img'),
+        // ('Hao', 'Yuan', '2005-11-01', 'someImg.img');
+        string values = "";
+        HttpResponseMessage response = client.GetAsync(parameters).Result;//wait until get the result
         if (response.IsSuccessStatusCode)
         {
             var data = response.Content.ReadAsAsync<IEnumerable<CharacterData>>().Result;
@@ -99,29 +105,25 @@ internal class Program
                     continue;
                 }
 
-                // ('Wenmian', 'Shi', '2005-11-01', 'someImg.img'),
-                // ('Pascal', 'Lefev', '2005-11-01', 'someImg.img'),
-                // ('Hao', 'Yuan', '2005-11-01', 'someImg.img');
-
                 if (values.Length != 0)
                 {
-                    values += @", ";
+                    values += ", ";
                 }
 
-                string firstName = @"";
-                string lastName = @"";
+                string firstName = "";
+                string lastName = "";
                 string[] name = d.Name.Split(' ');
                 if (name.Count() == 1)
                 {
                     firstName = name[0];
-                    lastName = @"Default lastname";
+                    lastName = "Default lastname";
                 }
                 else
                 {
                     firstName = name[0];
                     lastName = name[1];
                 }
-                values += @"('" + firstName + "', '" + lastName + "', '" + d.DateOfBirth + "', '" + d.Image + "')";
+                values += "('" + firstName + "', '" + lastName + "', '" + d.DateOfBirth + "', '" + d.Image + "')";
             }
 
             if (values.Length != 0)
@@ -135,7 +137,7 @@ internal class Program
         }
         client.Dispose();
 
-        // add some characters to my database
+        //add some characters to my database
         if (values.Length > 0)
         {
             command.CommandText = @"INSERT INTO characters (firstname, lastname, dateofbirth, image) Values " + values;
