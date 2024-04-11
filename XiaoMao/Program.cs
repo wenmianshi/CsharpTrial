3fc530fd135d7599b714574c5823586cfead11bc
wenmianshi <134840821+wenmianshi@users.noreply.github.com>
2024-04-11 21:58:18 +0200

    fill table with characters
    This fills the table of characters with some requested hogwarts characters

--- XiaoMao/Program.cs
+++ XiaoMao/Program.cs
@@ -64,6 +64,85 @@ internal class Program
         }
         dataReader.Close();
 
+        // Requesting some characters
+        HttpClient client = new HttpClient();
+        client.BaseAddress = new Uri("https://harry-potter-api-3a23c827ee69.herokuapp.com/api/characters");
+        string parameters = "";
+        client.DefaultRequestHeaders.Accept.Add(
+            new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json")
+           );
+
+        string values = @"";
+        HttpResponseMessage response = client.GetAsync(parameters).Result;
+        if (response.IsSuccessStatusCode)
+        {
+            var data = response.Content.ReadAsAsync<IEnumerable<CharacterData>>().Result;
+            foreach (var d in data)
+            {
+                if (d == null)
+                {
+                    continue;
+                }
+
+                if (d.Name == null || d.Name.Length == 0)
+                {
+                    continue;
+                }
+
+                if (d.DateOfBirth == null || d.DateOfBirth.Length == 0)
+                {
+                    continue;
+                }
+
+                if (d.Image == null || d.Image.Length == 0)
+                {
+                    continue;
+                }
+
+                // ('Wenmian', 'Shi', '2005-11-01', 'someImg.img'),
+                // ('Pascal', 'Lefev', '2005-11-01', 'someImg.img'),
+                // ('Hao', 'Yuan', '2005-11-01', 'someImg.img');
+
+                if (values.Length != 0)
+                {
+                    values += @", ";
+                }
+
+                string firstName = @"";
+                string lastName = @"";
+                string[] name = d.Name.Split(' ');
+                if (name.Count() == 1)
+                {
+                    firstName = name[0];
+                    lastName = @"Default lastname";
+                }
+                else
+                {
+                    firstName = name[0];
+                    lastName = name[1];
+                }
+                values += @"('" + firstName + "', '" + lastName + "', '" + d.DateOfBirth + "', '" + d.Image + "')";
+            }
+
+            if (values.Length != 0)
+            {
+                values += ";";
+            }
+        }
+        else
+        {
+            Console.WriteLine("Error");
+        }
+        client.Dispose();
+
+        // add some characters to my database
+        if (values.Length > 0)
+        {
+            command.CommandText = @"INSERT INTO characters (firstname, lastname, dateofbirth, image) Values " + values;
+            dataReader = command.ExecuteReader();
+            dataReader.Close();
+        }
+
         // Close databases physical connection
         currentConnection.Close();
     }
